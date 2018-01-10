beb6502fdf5611792bebef220aef775e5e83f465
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-01-10 17:00:11 +0530

    dharani.c

---
+++ dharani.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    double number;
+
+    printf("Enter a number: ");
+    scanf("%lf", &number);
+
+    if (number )
+    {
+        if (number)
+            printf("enter the positive number.");
+        else
+            printf(" enter the negative number.");
+    }
+    else
+        printf("the number is  positive number.");
+    return 0;
+}
