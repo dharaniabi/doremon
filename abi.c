873812ac319c1a1069088d98a6c5aa2b667c281e
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-01-17 16:42:09 +0530

    abi.c

---
+++ abi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(a=possitive or negative)
+{
+printf("|n the number is possitive ");
+else
+printf("|nthe number is negative");
+}
+printf("\n the number is possitive or negative");
+getch();
+}
