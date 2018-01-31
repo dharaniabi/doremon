e0c1426fba760794aa19db08660c0469a5a0fd55
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-01-31 16:26:27 +0530

    c program
    positive negative

---
+++ possitive.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n enter the value");
+scanf("\n %d",&n);
+if(n=odd)
+{
+printf("\n the number is possitive");
+else
+printf("\n the number is negative");
+}
+return 0;
+}
