6af8badc071f9eaf3f044938523240258e63123d
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-02 15:43:01 +0530

    dharanipositiv.c
    program

---
+++ dharanipositiv.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%f", &n);
+
+    if (n)
+    {
+        if (n=2)
+            printf("enter the positive number\n");
+        else
+            printf(" enter the negative number\n");
+    }
+    else
+        printf("the number is  positive number\n");
+    return 0;
+}
