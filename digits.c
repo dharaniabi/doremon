e9b79342a986b8184e614582006688601e5b4ec0
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-05 16:21:39 +0530

    digits.c
    number of digits

---
+++ digits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n,i,sum;
+float integer;
+printf("enter the number of digits");
+scanf("%d',&digits);
+for(i=0;i<=1;i++)
+{
+sum=n+i;
+}
+printf("\n sum of digits of integer:%d");
+return 0;
+}
