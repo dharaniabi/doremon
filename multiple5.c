fce08f533119f54385eae370a851285e8d7713e4
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-05 16:49:16 +0530

    multiple5.c
    n number five multiply

--- multiple5.c
+++ multiple5.c
@@ -3,8 +3,8 @@ void main()
 {
 int n,num1,num2,num3,num4,num5,sum;
 printf("enter the n numbers");
-scanf("%d",&n numberd);
-for(i=0;i<=n;i+=)
+scanf("%d",&n numbers);
+for(i=0;i<=n;i++)
 {
 sum=num1*num2*num3*num4*num5;
 }
