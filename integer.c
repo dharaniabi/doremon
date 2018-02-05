444b1a3fef7634ae551ee50b3b34862a81aa093e
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-05 16:12:30 +0530

    integer.c
    integer program

---
+++ integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i,sum;
+int array[N,K];
+printf("\n enter the arrays");
+scanf("%d",&array);
+for(i=0;i<=n;i++)
+{
+sum=n+k;
+}
+printf("\n sum of arrays:%d");
+return 0;
+}
