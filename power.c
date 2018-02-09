b0946cc87a9ae1d51d809fd4c6d95856cbdca785
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-09 15:46:31 +0530

    power.c
    power program

---
+++ power.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,sum,number;
+printf("\n enter the number");
+scanf("%d",&number);
+if(number=2)
+{
+sum=2^n;
+}
+printf("\n power of the number is %d":sum);
+return 0;
+}
