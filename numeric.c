2e1cd3480b2623c3b57954067e9bb4b64e0a2125
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-09 16:15:01 +0530

    numeric.c
    numeric program

---
+++ numeric.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char str[50],i,flag;
+printf("enter the string");
+scanf("%d",&string);
+if(flag==0)
+{
+  flag=0;
+  temp flag;
+  else
+  flag=1;
+  }
+  printf("\n the string is numeric");
+  return 0;
+  }
+
