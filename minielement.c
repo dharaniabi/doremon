1b0a6d8afbaca072887fcb52d1ce4de71786de4e
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-09 16:28:27 +0530

    minielement.c
    minimum element program

---
+++ minielement.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  char a[3],temp;
+  int i;
+  for(i=0;i<3;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  temp=a[0];
+  for(i=0;i<3;i++)
+  {
+    if(a[0]<a[i])
+    {
+      temp=a[i]);
+    }
+    printf("the smallest number is %d":temp);
+  }
+}
