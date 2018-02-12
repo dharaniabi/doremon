45fee819cf404dc889329249cd17955a9f7d0677
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-12 16:06:27 +0530

    narrayelement.c
    n  number andarray program

---
+++ narraynumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+int a[i];
+printf("enter the n numbers");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("enter the array n element");
+scanf("%d",&a[i]);
+}
+printf("the array n element %d",i,a[i]);
+return 0;
+}
