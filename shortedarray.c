322b5714f9fa617f1575ed6149d5d12329f12abf
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-12 16:41:49 +0530

    shortedarray.c
    shorted array program

---
+++ shortedarray.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+clrscr();
+inta[i];
+printf("enter the numbers");
+scanf("%d",&number);
+for(i=0;i<n;i++)
+{
+printf("the array is a[i]");
+}
+printf{"the shorted array is %d:i,a[i]);
+return 0;
+}
