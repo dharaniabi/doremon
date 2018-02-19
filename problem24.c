813005fbaeb29f1e7486b154f821eaa3350016bf
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-19 16:22:40 +0530

    problem24.c
    input constraints program

--- problem24.c
+++ problem24.c
@@ -1,12 +1,14 @@
+
 #include<stdio.h>
 int main()
 {
-int n,i,a[10];
+int n,i,a[10],number;
 printf("enter the number");
-scanf("%d",&n);
-for(i=0;i<n,i+=)
+scanf("%d",&number);
+for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
+printf("the input constraints are %d",&a[i]);
 return 0;
 }
