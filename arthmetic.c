97ec1f5e4b646ef53c36d547a5c3b1568f9b8aad
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-19 16:46:28 +0530

    arthmetic.c
    arthemetic progression program

---
+++ arthmetic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char a[5];
+int n,i,number;
+printf("enter the number");
+scanf("%d",&number);
+for(i=0;i<n;i++)
+{
+  if(a[i]+a[i+1])
+  {
+  	scanf("%d",a[i]);
+  }
+ else
+ scanf("%d",a[i+1])
+}
+}
