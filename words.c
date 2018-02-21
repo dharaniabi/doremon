abeb361c9424cf46b925b79d255fce275f6bb35e
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-21 16:36:35 +0530

    words.c
    number of words in a program

---
+++ words.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  char a[20];
+  int count=0,i;
+  printf("enter the line");
+  scanf("%d")
+  for(i=0;a[i]!=o;i++)
+  {
+   if(a[i]=' ');
+   count++;
+  }
+  printf("count the number words in a line")
+ return 0;
+}
+	
