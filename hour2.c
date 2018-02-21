4a8d0d0ee10f68e0952d4daebaa0b903e4df560c
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-21 16:02:31 +0530

    hour2.c
    hour and minute program

---
+++ hour2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int h1,h2,m1,m2,sum;
+int i,n;
+printf("\n enter the hour &time");
+scanf("%d%d",&h1,&m1);
+printf("\n enter thehour & time");
+scanf("%d%d",&h2,&m2);
+for(i=0;i<=n;i++)
+{
+sum=((h1+h2)/2)-((m1+m2)/2);
+}
+printf("sum is%d");
+return 0;
+}
