205ab11d5c2fe304089cbf1e42704643e4cbc638
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-21 16:54:39 +0530

    lines.c
    number of lines in a prograph

---
+++ lines.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+int main()
+{
+  char a[100];
+  int count=0,i;
+  printf("enter the line");
+  scanf("%d")
+  for(i=0;a[i]!=o;i++)
+  {
+   if(a[i]=' ');
+   count++;
+  }
+  printf("count the number line  in a paragraph")
+ return 0;
+}
