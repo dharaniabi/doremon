71c2df81677664f0e1a7869493b7b7d3f70dea3b
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-21 16:45:41 +0530

    space.c
    count number  of space in a line

---
+++ space.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+int main()
+{
+  char a[20];
+  int count=0,i;
+  printf("enter the line");
+  scanf("%d")
+  for(i=0;a[i]!=o;i++)
+  {
+   if(a[i]=' ');
+   count++;
+  }
+  printf("count the number space in a line")
+ return 0;
+}
