c0674dc2da06a2ac3fe46d635e7a6e479394ea1d
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-23 16:13:45 +0530

    maximum number.c
    maximum among 10 numbers

---
+++ maximum number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  char a[10];
+  int i;
+  printf("\n enter the 10 numbers");
+  scanf("%d",&10 numbers);
+  for(i=0;i<=10;i++)  
+  {
+    scanf("%d",&a[i]);
+  }
+  temp=a[0];
+  for(i=0;i<10;i++)
+  {
+    if(a[0]<a[i])
+    {
+      temp=a[i]);
+    }
+    printf("the maximum number is %d":temp);
+  }
+}
