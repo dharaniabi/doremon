251efc068ff5d4dc0c07532760c363e456092cfa
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-23 16:55:27 +0530

    power2.c
    power 2 of numbers

---
+++ power2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,sum,number;
+printf("\n enter the number");
+scanf("%d",&number);
+if(number=2)
+{
+sum=n^2;
+}
+printf("\n power of 2 number is %d":sum);
+return 0;
+}
