a10ae6c15e07e5e31b3d8877e35a3f578c56b698
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-26 16:47:27 +0530

    range.c
    the number is in range

---
+++ range.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the n numbers");
+scanf("%d",&n);
+if(n<10)
+{
+printf("the number n is in range");
+}
+else
+{
+printf("the number n is not in range");
+}
+return 0;
+}
