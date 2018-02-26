d6ff5c2017ea6ecadafe9ab441236355de79a592
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-02-26 16:29:38 +0530

    string.c
    string program

---
+++ string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char string[50];
+int i,k;
+printf("enter the string");
+scanf("%d",&string);
+for(i=0;i<=k;i++)
+{
+for(j=0;j<=i;j++)
+{
+printf("\n the string in k times is");
+}
+}
+return 0;
+}
