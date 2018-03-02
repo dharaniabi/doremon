4fc268857356545f4e1a649e1a3b4a0387f7b364
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-02 16:06:58 +0530

    product.c
    product of two numbers

---
+++ product.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int M,N,n,i,sum;
+printf("enter the first number");
+scanf("%d",&M);
+printf("enter the second number");
+scanf("%d",&N);
+for(i=0;i<n;i++)
+{
+sum=M+N;
+}
+if(sum=n/2)
+{
+printf("the sum is even");
+}
+else
+{
+printf("the sum is odd");
+}
+return 0;
+}
