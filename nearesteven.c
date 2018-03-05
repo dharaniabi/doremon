5bc3a1dc9ce1268064fb6ba0307d645b1c04c755
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-05 02:53:02 -0800

    nearesteven.c

---
+++ nearesteven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number ");
+scanf("%d",&n);
+if(n%2!=0)
+{
+a=n-1;
+if(a%2==0)
+{
+printf("the number is less than");
+}
+else
+{
+printf("the number is equal");
+}
+}
+return 0;
+}
