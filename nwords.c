251c5ca5e77d11c0b7218e663365c5befd8f3162
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-05 02:28:19 -0800

    nwords.c

---
+++ nwords.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+switch(num)
+{
+case1:
+printf("\n one");
+break;
+case2:
+printf("\n two");
+break;
+case3:
+printf("\n three");
+break;
+default:
+printf("\n nothing");
+}
+}
