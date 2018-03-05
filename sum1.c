22b5070e13e4724fea606a5de909e6889d48c3d8
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-05 02:32:20 -0800

    sum1.c

---
+++ sum1.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+int a,b,sum,i,n;
+printf("enter the two numbers");
+scanf("%d%d",&a,&b);
+for(i=0;i<n;i++)
+{
+sum=a+b;
+}
+printf("sum is %d");
+return 0;
+}
+
+
