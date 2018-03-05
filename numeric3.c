7dca426d889fde7f6ee43ba1009da01add11be56
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-05 03:15:02 -0800

    numeric3.c

---
+++ numeric3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int number,n,i,count;
+printf("enter the number");
+scanf("%d",&number);
+for(i=0;i<n;i++)
+{
+scanf("%d");
+count++;
+}
+printf("the number of digits %d",count);
+return 0;
+}
