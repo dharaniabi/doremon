f6e75aa8d82023539ec9762862b6d2dcb161403e
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-07 02:50:11 -0800

    average2.c

--- average2.c
+++ average2.c
@@ -2,8 +2,10 @@
 int main()
 {
 int n,i,sum,avg;
-printf("enter the n number");
+printf("enter the n");
 scanf("%d",&n);
+printf("enter the number");
+scanf("%d",num);
 for(i=0;i<n;i++)
 {
 sum=sum+i;
