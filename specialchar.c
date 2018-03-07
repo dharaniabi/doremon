d7906c0121eff5f3d5cacc31cf895e7322a0e450
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-07 03:16:47 -0800

    specialchar.c

---
+++ specialchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+char a[100]="abc123@$";
+int i,count=0;
+for(i=0;a[i]!='\0';i++)
+{
+if((a[i]>='a' && a[i]<='z') || (a[i]>='A' && a[i]<='Z'))
+{
+else if((a[i]>='0' && a[i]<='9'))
+{
+printf("no special character");
+}
+else
+{
+count++;
+}
+}
+printf("special character is %d",count);
+}
+return 0;
+}
