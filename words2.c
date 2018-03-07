bd9607fa133af27327c47148523ebf57fffcb144
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-07 03:21:37 -0800

    words2.c

---
+++ words2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  char a[20];
+  int count=0,i;
+  printf("enter the sentence");
+  scanf("%d")
+  for(i=0;a[i]!=o;i++)
+  {
+   if(a[i]=' ');
+   count++;
+  }
+  printf("count the number words in a sentence",count)
+ return 0;
+}
+	
