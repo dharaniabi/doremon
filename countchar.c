45c33d9ff003c7696d4bd9f639af32004b4ac38f
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-07 03:07:15 -0800

    countchar.c

---
+++ countchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[25];
+int i,n,count=0,m,a;
+printf("enter the character");
+scanf("%d",&a);
+m=strlen(a);
+for(i=0;i<m;i++)
+{
+printf("a[i]!=' ');
+count++;
+}
+printf("number of characters %d",count);
+return 0;
+}
