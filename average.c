6b4d3219c54fa59f9a2adc5428918cbb4d8b1183
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-07 02:43:24 -0800

    average.c

---
+++ average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,i,sum;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+sum=sum+i;
+}
+average=sum/2;
+printf("average value is %d":average);
+return 0;
+}
