cef6e657c94c16a72495d20adfb1f03ee572c43c
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-09 03:18:47 -0800

    odddigits.c

---
+++ odddigits.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,num;
+printf("enter the number");
+scanf("%d",&n);
+printf("enter the numbers");
+scanf("%d",&num);
+if(n%==2)
+{
+printf("odd numbers");
+}
+else
+{
+printf("even numbers");
+}
+return 0;
+}
