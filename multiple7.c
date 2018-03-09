93ac00d059b80e06eccd3b4a72f2c4eb52a7e5b8
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-09 03:22:35 -0800

    multiople7.c

---
+++ multiple7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("the number is multiple of 7");
+}
+else
+{
+printf("the number is not multiple of 7");
+}
+return 0;
+}
