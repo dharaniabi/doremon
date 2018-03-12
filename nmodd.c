2095c182b8b546b2786d70eac117d5779f101823
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-12 03:29:08 -0700

    nmodd.c

---
+++ nmodd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,n,sum,M,N;
+printf("enter the two numbers");
+scanf("%d%d",&M,&N);
+for(i=0;i<n;i++)
+{
+sum=M+N;
+}
+if(n%2==0)
+{
+printf("sum is even");
+}
+else
+{
+printf("sum is odd");
+}
+}
