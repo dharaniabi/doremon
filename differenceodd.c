7d36ff9c592b17d6beee6c21b9a4401eb0d01394
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-12 03:34:44 -0700

    differenceodd.c

---
+++ differenceodd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,n,sum,a,b;
+printf("enter the two numbers");
+scanf("%d%d",&a,&b);
+for(i=0;i<n;i++)
+{
+sum=a-b;
+}
+if(n%2==0)
+{
+printf("sum is even");
+}
+else
+{
+printf("sum is odd");
+}
+}
