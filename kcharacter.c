b59361dc0c5b692619f74dd8e22d5d8d5e4d33dd
dharaniabi <31438777+dharaniabi@users.noreply.github.com>
2018-03-28 10:36:38 +0000

    kcharacter.c

---
+++ kcharacter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char s[20];
+    int n,i;
+    printf("enter the string");
+    scanf("%d",&s[i]);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%c",s[i]);
+    }
+    return 0;
+}
